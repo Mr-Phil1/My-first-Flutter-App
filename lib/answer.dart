c58e4fd323e977f1f3e5808dcd30476248aee994
Mr. Phil <mr.phil1@gmx.net>
2021-10-29 19:08:10 +0200

    modify answer.dart * style the button * add an selectHandler for a function

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+
 class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+
+  Answer(this.selectHandler);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: EdgeInsets.all(2),
-      color: Colors.blue,
       child: ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: null,
-            ),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+        child: Text('Answer 1'),
+        onPressed: selectHandler,
+      ),
     );
   }
 }
