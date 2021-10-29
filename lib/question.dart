3efa200b198726ad5dfb9eead10da8dfe287f86e
Mr. Phil <mr.phil1@gmx.net>
2021-10-29 18:08:30 +0200

    modify question.dart * add a Question Constructor

--- lib/question.dart
+++ lib/question.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
+
+String questionText;
+
+Question(this.questionText);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Text(questionText);
   }
 }
