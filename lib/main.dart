807f329ce873adb47a00f179bcb3c2f688262cc4
Mr. Phil <mr.phil1@gmx.net>
2021-10-29 16:54:43 +0200

    -

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My First App'),
+          title:  Text('My First App'),
         ),
-        body: const Text('This is my default text!'),
+        body:  Text('This is my default text!'),
       ),
     );
   }
