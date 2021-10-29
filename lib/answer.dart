1b1542de75fbb129f9b11875943b9e50e5ede8e3
Mr. Phil <mr.phil1@gmx.net>
2021-10-29 18:41:09 +0200

    modify answer.dart * add the ElevatedButton from main.dart

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,7 +3,13 @@ class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      margin: EdgeInsets.all(2),
+      color: Colors.blue,
+      child: ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
     );
   }
 }
